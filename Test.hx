8a60d8d371d69309635abec4818a7f9a062aac9d
Rudy G <k@klabz.org>
2021-07-07 12:27:07 +0200

    Add more variable declaration tests

--- Test.hx
+++ Test.hx
@@ -114,7 +114,11 @@ class Test extends TestCase {
 		assertScript("var a = 10; var b = 5; a - -b", 15);
 		assertScript("var a = 10; var b = 5; a - b / 2", 7.5);
 		assertScript("var a; a", null);
+		assertScript("var a = 1, b = 5; a + b;", 6);
 		assertScript("var a, b = 5; if (a == null) a = 2; a + b;", 7);
+		assertScript("var a:Int; a", null, null, true);
+		assertScript("var a:Int = 1, b:Int = 5; a + b;", 6, null, true);
+		assertScript("var a:Int, b:Int = 5; if (a == null) a = 2; a + b;", 7, null, true);
 	}
 
 	function testMap():Void {
