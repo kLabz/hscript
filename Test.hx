3542be987a135d0e6d67e036c176d09d91c8f87a
Rudy G <k@klabz.org>
2021-07-07 12:08:59 +0200

    Disallow arbitrary operators in variable declaration

--- Test.hx
+++ Test.hx
@@ -113,6 +113,8 @@ class Test extends TestCase {
 		assertScript("var f:(x:Int)->(Int, Int)->Int = (x:Int) -> (y:Int, z:Int) -> x + y + z; f(3)(1, 2)", 6, null, true);
 		assertScript("var a = 10; var b = 5; a - -b", 15);
 		assertScript("var a = 10; var b = 5; a - b / 2", 7.5);
+		assertScript("var a; a", null);
+		assertScript("var a, b = 5; if (a == null) a = 2; a + b;", 7);
 	}
 
 	function testMap():Void {
